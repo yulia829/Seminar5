ef42d3630700ce34ccde66d9e3db7af1d2499990
Yulia <meshokyulia0002@gmail.com>
2023-09-03 12:12:44 +0300

    hw51

---
+++ hw5_1/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Введите количество элементов массива:" );
+int a = Convert.ToInt32(Console.ReadLine());
+int[] randomArray = new int[a];
+mas(a);
+void mas (int a)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < a; i++)
+{
+randomArray[i] = new Random().Next(99,999);
+Console.Write(randomArray[i] + "  "  );
+
+}
+Console.Write("] ");
+}
+
+int count(int[] randomArray)
+{
+int count = 0;
+for (int i = 0; i < randomArray.Length; i++)
+{
+if (randomArray[i] % 2 == 0)
+count = count + 1;
+}
+return count;
+}
+
+Console.Write($"- Количество чётных чисел в массиве: {count(randomArray)}");
