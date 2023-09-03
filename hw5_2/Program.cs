5cc5f5641153485acf2ed4c4f7223d3427f79372
Yulia <meshokyulia0002@gmail.com>
2023-09-03 12:23:25 +0300

    hw52

---
+++ hw5_2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите размер массива:");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(-100,100);
+             Console.Write(numbers[i] + " ");
+        }
+}
+int sum = 0;
+for (int i = 1; i < numbers.Length; i=i+2)
+    sum = sum + numbers[i];
+Console.Write("Сумма элементов:" +  sum);
+
+
